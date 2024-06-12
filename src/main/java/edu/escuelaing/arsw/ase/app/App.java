6ed63c2a0907cf2c067613991679a886556bda73
Parralol <santinico36@hotmail.com>
2024-06-11 23:26:20 -0500

    minor changes

--- src/main/java/edu/escuelaing/arsw/ase/app/App.java
+++ src/main/java/edu/escuelaing/arsw/ase/app/App.java
@@ -1,7 +1,5 @@
 package edu.escuelaing.arsw.ase.app;
 
-import java.io.IOException;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -19,17 +17,29 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
-     * @throws IOException 
+     * @throws Exception 
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         List<Path> allFiles = new ArrayList<>();
+        List<String> files = new ArrayList<>();
+
         System.out.println("Hello World!");
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        
         String xd = System.getProperty("user.dir");
         Path rootPath = Paths.get(xd.split(":")[0] + ":", xd.split(":")[1]);
         allFiles = Lister.listAllFiles(rootPath, allFiles); 
-  
+
+        for(Path a : allFiles){
+            Path con = a.getFileName();
+            if(con.toString().equals(args[1])){
+                files.add(a.toString());
+            }
+        }
+        if(files.size()>1) throw new Exception("There is more than 1 file with the same name");
         System.out.println("Found files:"); 
         allFiles.forEach(System.out::println); 
+
+
     }
 }
--- src/main/java/edu/escuelaing/arsw/ase/app/phy.java
+++ src/main/java/edu/escuelaing/arsw/ase/app/phy.java
@@ -1,5 +1,17 @@
 package edu.escuelaing.arsw.ase.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class phy {
-    
+        public String[] calculate(String name){
+        try{
+            File file = new File(name);
+            Scanner reader = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
